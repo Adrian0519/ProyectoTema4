0315dcbcb7ebc328496981973a726a6469de3488
Adrián <adrian.rodriguez.roqueiro@gmail.com>
2025-03-17 13:09:44 +0100

    Realizado el metodo 3 y cambiada la forma de conectar

--- src/ConsultasXml/ConXml.java
+++ src/ConsultasXml/ConXml.java
@@ -6,27 +6,60 @@ import java.io.IOException;
 
 public class ConXml {
 
-    public void remplazar(BaseXClient conexion,int id,String remplazo, String datoRemplazo){
+    private static BaseXClient session;
+
+    public void remplazar(int id,String remplazo, String datoRemplazo){
         try {
             String consulta = String.format("""
-                for $v in db:get('videojuegos')//videojuegos/videojuego[id=%d]
+                for $v in /videojuegos/videojuego[id=%d]
                 return replace value of node $v/%s with '%s'
             """, id, remplazo, datoRemplazo);
-            conexion.execute("xquery" + consulta);
+            session.execute("xquery" + consulta);
             System.out.println("Cambios aplicados");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void eliminarXID(BaseXClient conexion, int id){
+    public void eliminarXID(int id){
         try {
             String consulta = String.format("""
-                for $videojuegos in db:get('videojuegos')//videojuegos/videojuego[id=%d]
+                for $videojuegos in /videojuegos/videojuego[id=%d]
                 return delete node $videojuegos
                 """, id);
-            conexion.execute("xquery " + consulta);
+            session.execute("xquery " + consulta);
             System.out.println("Juego eliminado");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void filtrarVideojuegos(){
+        try {
+        String consulta= String.format("""
+                  for $videojuego in /videojuegos/videojuego
+                            order by $videojuego/plataforma, $videojuego/titulo
+                            return <videojuego>
+                                { $videojuego/id }
+                                { $videojuego/titulo }
+                                { $videojuego/precio }
+                                { $videojuego/disponibilidad }
+                                { $videojuego/edad_minima_recomendada }
+                                { $videojuego/plataforma }
+                            </videojuego>
+                """);
+        String datos=session.execute("xquery " + consulta);
+        System.out.println(datos);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ConXml() {
+        try {
+            BaseXClient baseXClient = new BaseXClient("localhost", 1984, "admin", "abc123");
+            baseXClient.execute("open videojuegos");
+            this.session=baseXClient;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/Main.java
+++ src/Main.java
@@ -6,175 +6,165 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        ConXml conXml=new ConXml();
-        int opcion=0;
-        try (BaseXClient baseXClient=new BaseXClient("localhost",1984,"admin","abc123")){
-        while (opcion!=19) {
-            System.out.println("1.-Modificar el valor de un elemento de un XML según un ID.\n" +
-                    "2.-Eliminar un videojuego según su ID.\n" +
-                    "3.-Obtener todos los videojuegos ordenados por plataforma y en segundo lugar por título (se mostrarán los siguientes campos: id, titulo, precio, disponibilidad, edad_minima_recomendada y plataforma).\n" +
-                    "4.-Listar videojuegos con una edad_minima_recomendada menor o igual a X años (se mostrarán los siguientes campos: id, titulo, precio, disponibilidad, edad_minima_recomendada y plataforma). Se deberá mostrar la información ordenada según la edad_minima_recomendada.\n" +
-                    "5.-Mostrar la plataforma, el titulo y el precio del videojuego más barato para cada plataforma. En el caso de haber varios se devolverá el de la primera posición.\n" +
-                    "6.-Mostrar el titulo y el genero de aquellos videojuegos cuya descripcion incluya una subcadena, independientemente del uso de mayúsculas o minúsculas. Se deberá mostrar la información ordenada alfabéticamente según el genero.\n" +
-                    "7.-Mostrar la cantidad total de videojuegos para cada plataforma (teniendo en cuenta el elemento disponibilidad) y calcular el porcentaje que representa respecto al total de videojuegos. Se deberá mostrar la información ordenada de forma descendente por la cantidad de videojuegos.\n" +
-                    "8.-Mostrar el precio que costaría comprar todos los videojuegos disponibles (teniendo en cuenta el precio de cada videojuego y la disponibilidad de cada uno).\n" +
-                    "9.-Crear un nuevo usuario (no podrá haber email repetidos).\n" +
-                    "10.-Identificar usuario según el email. Dado el email se obtendrá el id del usuario de forma que las siguientes consultas se harán sobre ese usuario. Para cambiar de usuario se tendrá que volver a seleccionar esta opción.\n" +
-                    "11.-Borrar un usuario.\n" +
-                    "12.-Modificar el valor de un campo de la información del usuario.\n" +
-                    "13.-Añadir videojuegos al carrito del usuario. Se mostrará la lista de videojuegos cuya edad_minima_recomendada sea inferior o igual a la del usuario actual y se pedirá: id del videojuego y cantidad, así como si se desea seguir introduciendo más videojuegos.\n" +
-                    "14.-Mostrar el carrito del usuario. Se mostrarán los datos del carrito y el coste total.\n" +
-                    "15.-Comprar el carrito del usuario. Se mostrará el contenido del carrito junto con una orden de confirmación. Si la orden es positiva se pasarán todos los videojuegos a formar parte de una nueva compra y desaparecerán del carrito.\n" +
-                    "16.-Mostrar las compras del usuario, incluyendo la información de la fecha de cada compra.\n" +
-                    "17.-Teniendo en cuenta todos los usuarios, calcular el coste de cada carrito y listar los resultados ordenados por el total de forma descendente.\n" +
-                    "18.-Teniendo en cuenta todos los usuarios, calcular el total gastado por cada usuario en todas sus compras y listar los resultados ordenados por el total de forma ascendente.\n"+
-                    "19.-salir");
-            opcion= scanner.nextInt();
-            scanner.nextLine();
-            switch (opcion){
-                case 1:
-                    System.out.println("Pasame el id");
-                    int id=scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("Dime el parametro que quieras remplazar");
-                    System.out.println("1-titulo" +
-                            "2-descripcion" +
-                            "3-precio" +
-                            "4-disponibilidad" +
-                            "5-genero" +
-                            "6-desarrollador" +
-                            "7-edad minima" +
-                            "8-plataforma" +
-                            "0-salir");
-                    int remplazoElegir= scanner.nextInt();
-                    scanner.nextLine();
-                    String valorElegido;
-                    String nuevoDato;
-                    switch (remplazoElegir){
-                        case 1:
-                            valorElegido="titulo";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 2:
-                            valorElegido="descripcion";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 3:
-                            valorElegido="precio";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 4:
-                            valorElegido="disponibilidad";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 5:
-                            valorElegido="genero";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 6:
-                            valorElegido="desarrollador";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 7:
-                            valorElegido="edad_minima_recomendada";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 8:
-                            valorElegido="plataforma";
-                            System.out.println("Dime el valor que tendra ahora");
-                            nuevoDato= scanner.nextLine();
-                            conXml.remplazar(baseXClient,id,valorElegido,nuevoDato);
-                            break;
-                        case 0:
-                            System.out.println("Adios");
-                            break;
-                        default:
-                            System.out.println("Dato no correcto");
-                            break;
-                    }
-                    break;
-                case 2:
-                    System.out.println("Inserta la id que deseas eliminar");
-                    int idEliminar=scanner.nextInt();
-                    scanner.nextLine();
-                    conXml.eliminarXID(baseXClient,idEliminar);
-                    break;
-                case 3:
-                    System.out.println("");
-                    break;
-                case 4:
-                    System.out.println("");
-                    break;
-                case 5:
-                    System.out.println("");
-                    break;
-                case 6:
-                    System.out.println("");
-                    break;
-                case 7:
-                    System.out.println("");
-                    break;
-                case 8:
-                    System.out.println("");
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    break;
-                case 12:
-                    break;
-                case 13:
-                    break;
-                case 14:
-                    break;
-                case 15:
-                    break;
-                case 16:
-                    break;
-                case 17:
-                    break;
-                case 18:
-                    break;
-                case 19:
-                    System.out.println("Adios");
-                    break;
-                default:
-                    System.out.println("Numero no valido");
+        Scanner scanner = new Scanner(System.in);
+        ConXml conXml = new ConXml();
+        int opcion = 0;
+            while (opcion != 19) {
+                System.out.println("1.-Modificar el valor de un elemento de un XML según un ID.\n" +
+                        "2.-Eliminar un videojuego según su ID.\n" +
+                        "3.-Obtener todos los videojuegos ordenados por plataforma y en segundo lugar por título (se mostrarán los siguientes campos: id, titulo, precio, disponibilidad, edad_minima_recomendada y plataforma).\n" +
+                        "4.-Listar videojuegos con una edad_minima_recomendada menor o igual a X años (se mostrarán los siguientes campos: id, titulo, precio, disponibilidad, edad_minima_recomendada y plataforma). Se deberá mostrar la información ordenada según la edad_minima_recomendada.\n" +
+                        "5.-Mostrar la plataforma, el titulo y el precio del videojuego más barato para cada plataforma. En el caso de haber varios se devolverá el de la primera posición.\n" +
+                        "6.-Mostrar el titulo y el genero de aquellos videojuegos cuya descripcion incluya una subcadena, independientemente del uso de mayúsculas o minúsculas. Se deberá mostrar la información ordenada alfabéticamente según el genero.\n" +
+                        "7.-Mostrar la cantidad total de videojuegos para cada plataforma (teniendo en cuenta el elemento disponibilidad) y calcular el porcentaje que representa respecto al total de videojuegos. Se deberá mostrar la información ordenada de forma descendente por la cantidad de videojuegos.\n" +
+                        "8.-Mostrar el precio que costaría comprar todos los videojuegos disponibles (teniendo en cuenta el precio de cada videojuego y la disponibilidad de cada uno).\n" +
+                        "9.-Crear un nuevo usuario (no podrá haber email repetidos).\n" +
+                        "10.-Identificar usuario según el email. Dado el email se obtendrá el id del usuario de forma que las siguientes consultas se harán sobre ese usuario. Para cambiar de usuario se tendrá que volver a seleccionar esta opción.\n" +
+                        "11.-Borrar un usuario.\n" +
+                        "12.-Modificar el valor de un campo de la información del usuario.\n" +
+                        "13.-Añadir videojuegos al carrito del usuario. Se mostrará la lista de videojuegos cuya edad_minima_recomendada sea inferior o igual a la del usuario actual y se pedirá: id del videojuego y cantidad, así como si se desea seguir introduciendo más videojuegos.\n" +
+                        "14.-Mostrar el carrito del usuario. Se mostrarán los datos del carrito y el coste total.\n" +
+                        "15.-Comprar el carrito del usuario. Se mostrará el contenido del carrito junto con una orden de confirmación. Si la orden es positiva se pasarán todos los videojuegos a formar parte de una nueva compra y desaparecerán del carrito.\n" +
+                        "16.-Mostrar las compras del usuario, incluyendo la información de la fecha de cada compra.\n" +
+                        "17.-Teniendo en cuenta todos los usuarios, calcular el coste de cada carrito y listar los resultados ordenados por el total de forma descendente.\n" +
+                        "18.-Teniendo en cuenta todos los usuarios, calcular el total gastado por cada usuario en todas sus compras y listar los resultados ordenados por el total de forma ascendente.\n" +
+                        "19.-salir");
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Pasame el id");
+                        int id = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("Dime el parametro que quieras remplazar");
+                        System.out.println("1-titulo" +
+                                "2-descripcion" +
+                                "3-precio" +
+                                "4-disponibilidad" +
+                                "5-genero" +
+                                "6-desarrollador" +
+                                "7-edad minima" +
+                                "8-plataforma" +
+                                "0-salir");
+                        int remplazoElegir = scanner.nextInt();
+                        scanner.nextLine();
+                        String valorElegido;
+                        String nuevoDato;
+                        switch (remplazoElegir) {
+                            case 1:
+                                valorElegido = "titulo";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar(id, valorElegido, nuevoDato);
+                                break;
+                            case 2:
+                                valorElegido = "descripcion";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 3:
+                                valorElegido = "precio";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 4:
+                                valorElegido = "disponibilidad";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 5:
+                                valorElegido = "genero";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 6:
+                                valorElegido = "desarrollador";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 7:
+                                valorElegido = "edad_minima_recomendada";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 8:
+                                valorElegido = "plataforma";
+                                System.out.println("Dime el valor que tendra ahora");
+                                nuevoDato = scanner.nextLine();
+                                conXml.remplazar( id, valorElegido, nuevoDato);
+                                break;
+                            case 0:
+                                System.out.println("Adios");
+                                break;
+                            default:
+                                System.out.println("Dato no correcto");
+                                break;
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Inserta la id que deseas eliminar");
+                        int idEliminar = scanner.nextInt();
+                        scanner.nextLine();
+                        conXml.eliminarXID( idEliminar);
+                        break;
+                    case 3:
+                        conXml.filtrarVideojuegos();
+                        break;
+                    case 4:
+                        System.out.println("");
+                        break;
+                    case 5:
+                        System.out.println("");
+                        break;
+                    case 6:
+                        System.out.println("");
+                        break;
+                    case 7:
+                        System.out.println("");
+                        break;
+                    case 8:
+                        System.out.println("");
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                    case 19:
+                        System.out.println("Adios");
+                        break;
+                    default:
+                        System.out.println("Numero no valido");
 
+                }
             }
-        }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
-    /**
-     *   private static BaseXClient conexion(){
-     *         try (BaseXClient baseXClient=new BaseXClient("localhost",1984,"admin","abc123")){
-     *             baseXClient.execute("open videojuegos");
-     *             return baseXClient;
-     *         } catch (IOException e) {
-     *             throw new RuntimeException(e);
-     *         }
-     *     }
-     * @return
-     */
 
-}
+
+
+
+
+
